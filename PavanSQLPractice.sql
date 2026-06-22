03f63963d97de96357521072bcb60f18351e28f7
Pavan Namballa <vamsman1234@gmail.com>
2026-06-22 11:43:22 -0400

    Semicolon added to DDL and DCL

--- PavanSQLPractice.sql
+++ PavanSQLPractice.sql
@@ -4,17 +4,24 @@
 
 -- Create Logs Table 
 
-Create table If Not Exists Logs (id int, num int)
-Truncate table Logs
-insert into Logs (id, num) values ('1', '1')
-insert into Logs (id, num) values ('2', '1')
-insert into Logs (id, num) values ('3', '1')
-insert into Logs (id, num) values ('4', '2')
-insert into Logs (id, num) values ('5', '1')
-insert into Logs (id, num) values ('6', '2')
-insert into Logs (id, num) values ('7', '2')
+Create table If Not Exists Logs (id int, num int);
+Truncate table Logs;
+insert into Logs (id, num) values ('1', '1');
+insert into Logs (id, num) values ('2', '1');
+insert into Logs (id, num) values ('3', '1');
+insert into Logs (id, num) values ('4', '2');
+insert into Logs (id, num) values ('5', '1');
+insert into Logs (id, num) values ('6', '2');
+insert into Logs (id, num) values ('7', '2');
 
--- Below Line of Code is for testing
-SELECT id, num,
-id - ROW_NUMBER() OVER (PARTITION BY num order by id) as GRP
-from logs;
+-- Below Line of Code is for testing 1
+WITH GROUPED AS(
+    SELECT id, num,
+    id - ROW_NUMBER() OVER (PARTITION BY num order by id) as GRP
+    from logs;
+),
+COUNTED AS(
+    SELECT num, grp, COUNT(*) as consecutive_count
+    from GROUPED 
+    GROUP BY num, grp
+)
